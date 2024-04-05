d4b1d13af2b0ed7517f70069a699ba28c2eeb8e7
Eduardo silva <eduardo2207@hotmail.com>
2024-04-05 11:12:54 -0300

    Consulta simples com query methods

--- src/main/java/com/workshopmongo/services/PostService.java
+++ src/main/java/com/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,8 @@ public class PostService {
 	public Post findByid(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objecto não encontrado."));
+	}
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
